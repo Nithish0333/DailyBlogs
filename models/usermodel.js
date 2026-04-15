b358d55352c2ab7420b86902757390c0759f6d21
NITHISH <163642384+Nithish0333@users.noreply.github.com>
2026-04-15 22:14:24 +0530

    Add user model with create and findByEmail methods

---
+++ models/usermodel.js
@@ -0,0 +1,15 @@
+const db = require("../config/db");
+
+const User = {
+  create: (name, email, password, callback) => {
+    const sql = "INSERT INTO users (name, email, password) VALUES (?, ?, ?)";
+    db.query(sql, [name, email, password], callback);
+  },
+
+  findByEmail: (email, callback) => {
+    const sql = "SELECT * FROM users WHERE email = ?";
+    db.query(sql, [email], callback);
+  }
+};
+
+module.exports = User;
